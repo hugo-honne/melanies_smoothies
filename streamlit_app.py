d7089b87322da513cc5ddd6f840f35c36ac0cbbf
hugo-honne <hugo.delacruz@honne.com.mx>
2024-06-17 13:07:56 -0600

    Update streamlit_app.py
    elimine lineas en blanco (org codigo)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-#from streamlit_snowflake import SnowflakeConnection
-
-# Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
