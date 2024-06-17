937bbfcd10e1cda5d425fc83fb2cf9d72f3cd951
hugo-honne <hugo.delacruz@honne.com.mx>
2024-06-17 16:42:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-	fv_df =st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+	    fv_df =st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders values 
              (smoothies.public.order_seq.nextval, false,'""" + ingredients_string + """','"""+name_on_order+ """', current_timestamp)"""
